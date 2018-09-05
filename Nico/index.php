70dfab7e26213ec19aef92bb793a012fa6b4db19
fluoroom <nico.vial.giudici@gmail.com>
2018-09-05 18:40:58 -0300

    kjufedchsakhl

--- Nico/index.php
+++ Nico/index.php
@@ -1,2 +1,12 @@
-<?php
-echo "aaah ahora sí";
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Santander Rio @ hery</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+</head>
+<body>
+	<p>aaaaaaaaaa</p>
+</body>
+</html>
