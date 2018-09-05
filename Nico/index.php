21a1af5debc2f0eaf99d17d663f2e62f2b16dcf5
fluoroom <nico.vial.giudici@gmail.com>
2018-09-05 19:21:15 -0300

    xd

--- Nico/index.php
+++ Nico/index.php
@@ -7,6 +7,35 @@
 	<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
 </head>
 <body>
-	<p>aaaaaaaaaa</p>
+	<head>
+		<img src="img/logo.png">
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>FAQ</li>
+			</ul>
+		</nav>
+	</head>
+	<main>
+		<section>
+			<h2>Registrate</h2>
+			<form method="POST">
+				<input type="email" name="email" placeholder="E-mail">
+				<input type="password" name="password" placeholder="Contraseña">
+				<input type="password" name="password" placeholder="Re-ingresar contraseña">
+				<input type="submit" value="&#8594;">
+			</form>
+		</section>
+		<section>
+			<h2>Ingresar</h2>
+			<form method="POST">
+				<input type="email" name="email" placeholder="E-mail o usuario">
+				<input type="password" name="password" placeholder="Contraseña">
+				<input type="checkbox" name="recordar">
+				<input type="submit" value="&#8594;">
+			</form>
+			<p><a href="resetpsw.php">Olvidé mi contraseña</a></p>
+		</section>
+	</main>
 </body>
 </html>
