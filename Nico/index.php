bc79f3324231d3501bccf20839d9589478f705e7
fluoroom <nico.vial.giudici@gmail.com>
2018-09-06 00:05:38 -0300

    home

--- Nico/index.php
+++ Nico/index.php
@@ -3,19 +3,19 @@
 <head>
 	<title>Santander Rio @ hery</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
 	<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
-	<head>
-		<img src="img/logo.jpg">
+	<header>
+		<span class="logo"><img src="img/logo.jpg" alt="logo"></span>
 		<nav>
 			<ul>
-				<li>Home</li>
-				<li>FAQ</li>
+				<li><a>Home</a></li>
+				<li><a>FAQ</a></li>
 			</ul>
 		</nav>
-	</head>
+	</header>
 	<main>
 		<section>
 			<h2>Registrate</h2>
@@ -23,7 +23,7 @@
 				<input type="email" name="email" placeholder="E-mail">
 				<input type="password" name="password" placeholder="Contraseña">
 				<input type="password" name="password" placeholder="Re-ingresar contraseña">
-				<input type="submit" value="&#8594;">
+				<input type="submit" value="&#11157;" class="submit">
 			</form>
 		</section>
 		<section>
@@ -32,7 +32,7 @@
 				<input type="email" name="email" placeholder="E-mail o usuario">
 				<input type="password" name="password" placeholder="Contraseña">
 				<input type="checkbox" name="recordar">
-				<input type="submit" value="&#8594;">
+				<input type="submit" value="&#11157;" class="submit">
 			</form>
 			<p><a href="resetpsw.php">Olvidé mi contraseña</a></p>
 		</section>
